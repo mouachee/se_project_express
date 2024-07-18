aba114a590e0cb8bbd2ac76d4101a0f1e929d8db
mouacheethao <thaomuajtsim005@gmail.com>
2024-07-18 09:29:03 -0500

    updates

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ mongoose
     console.log("Connect to DB");
   })
   .catch(console.error);
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133", // paste the _id of the test user created in the previous step
+  };
+  next();
+});
 app.use(express.json());
 app.use("/", mainRouter);
 
