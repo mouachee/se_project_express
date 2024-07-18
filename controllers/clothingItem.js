6301b13a713012253a1964f0f8fd1e22b5be72e8
mouacheethao <thaomuajtsim005@gmail.com>
2024-07-17 20:23:04 -0500

    finished

--- controllers/clothingItem.js
+++ controllers/clothingItem.js
@@ -4,9 +4,9 @@ const createItem = (req, res) => {
   console.log(req);
   console.log(res.body);
 
-  const { name, weather, imageURL } = req.body;
+  const { name, weather, imageUrl } = req.body;
 
-  ClothingItem.create({ name, weather, imageURL })
+  ClothingItem.create({ name, weather, imageUrl })
     .then((item) => {
       console.log(item);
       res.send({ data: item });
@@ -15,6 +15,39 @@ const createItem = (req, res) => {
       return res.status(500).send({ message: "Error from createItem", err });
     });
 };
+const getItems = (req, res) => {
+  ClothingItem.find({})
+    .then((items) => res.status(200).send(items))
+    .catch((err) => {
+      res.status(500).send({ message: "Error from getItems" }, err);
+    });
+};
+
+const updateItems = (req, res) => {
+  const { itemId } = req.params;
+  const { imageUrl } = req.body;
+
+  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageUrl } })
+    .orFail()
+    .then((item) => res.status(200).send({ data: item }))
+    .catch((err) => {
+      res.status(500).send({ message: "Error from updateItems" }, err);
+    });
+};
+const deleteItems = (req, res) => {
+  const { itemId } = req.params;
+
+  console.log(itemId);
+  ClothingItem.findByIdAndDelete(itemId)
+    .orFail()
+    .then((item) => res.status(204).send({}))
+    .catch((err) => {
+      res.status(500).send({ message: "Error from deleteItems" }, err);
+    });
+};
 module.exports = {
   createItem,
+  getItems,
+  updateItems,
+  deleteItems,
 };
--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -1,9 +1,25 @@
 const router = require("express").Router();
 
-const { createItem } = require("../controllers/clothingItem");
+const {
+  createItem,
+  getItems,
+  updateItems,
+  deleteItems,
+} = require("../controllers/clothingItem");
 //CRUD
 
 //Create
 router.post("/", createItem);
+
+//Read
+
+router.get("/", getItems);
+// Update
+
+router.put("/:itemId", updateItems);
+
+//Delete
+
+router.delete("/:itemId", deleteItems);
 
 module.exports = router;
