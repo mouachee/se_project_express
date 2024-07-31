9bae056c2e9c748e7393a7d31b6264a438a58837
mouacheethao <thaomuajtsim005@gmail.com>
2024-07-31 16:20:51 -0500

    added email require

--- controllers/users.js
+++ controllers/users.js
@@ -26,6 +26,12 @@ const getUsers = (req, res) => {
 const createUser = (req, res) => {
   const { name, avatar, email, password } = req.body;
 
+  if (!email) {
+    return res
+      .status(BAD_REQUEST)
+      .send({ message: "An email address is required." });
+  }
+
   User.findOne({ email })
     .then((existingEmail) => {
       if (existingEmail) {
@@ -36,29 +42,28 @@ const createUser = (req, res) => {
       return bcrypt.hash(password, 10);
     })
     .then((hash) => {
-      User.create({ name, avatar, email, password: hash })
-        .then((user) => {
+      return User.create({ name, avatar, email, password: hash }).then(
+        (user) => {
           res.status(201).send({
             name: user.name,
             avatar: user.avatar,
             email: user.email,
           });
-        })
-        .catch((err) => {
-          if (err.code === 11000) {
-            return res
-              .status(CONFLICT)
-              .send({ message: "Email already exist" });
-          }
-          if (err.name === "ValidationError") {
-            return res
-              .status(BAD_REQUEST)
-              .send({ message: "Error from createuser" });
-          }
-          return res
-            .status(INTERNAL_SERVER_ERROR)
-            .send({ message: "An error has occurred on the server" });
-        });
+        }
+      );
+    })
+    .catch((err) => {
+      if (err.code === 11000) {
+        return res.status(CONFLICT).send({ message: "Email already exist" });
+      }
+      if (err.name === "ValidationError") {
+        return res
+          .status(BAD_REQUEST)
+          .send({ message: "Error from createuser" });
+      }
+      return res
+        .status(INTERNAL_SERVER_ERROR)
+        .send({ message: "An error has occurred on the server" });
     });
 };
 
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const { getCurrentUser } = require("../controllers/users");
 const auth = require("../middlewares/auth");
 
-router.get("/users/me", auth, getCurrentUser);
+router.get("/me", auth, getCurrentUser);
 
 module.exports = router;
