59d925055af94d0dad4292573e078a3a5250b434
mouacheethao <thaomuajtsim005@gmail.com>
2024-10-03 09:04:35 -0500

    pass next to handeAuthError

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -11,7 +11,7 @@ const authMiddleware = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    return handleAuthError(res);
+    return handleAuthError(next);
   }
   const token = authorization.replace("Bearer ", "");
   let payload;
@@ -19,7 +19,7 @@ const authMiddleware = (req, res, next) => {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     console.error(err);
-    return handleAuthError(res);
+    return handleAuthError(next);
   }
   req.user = payload;
   return next();
